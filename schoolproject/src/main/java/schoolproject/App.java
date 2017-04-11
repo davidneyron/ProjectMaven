6a70748200786e2230f3c93f03a8499f9030a1ea
David <david.neyron@gmail.com>
2017-04-11 18:00:18 +0200

    update

--- schoolproject/src/main/java/schoolproject/App.java
+++ schoolproject/src/main/java/schoolproject/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(max(4,5));
+    }
+    
+    public static int max(int a, int b){
+    	return a > b ? a : b;
     }
 }

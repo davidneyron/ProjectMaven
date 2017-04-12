99499b776ea968460d08362f0fcf7aaa2f516135
David <david.neyron@gmail.com>
2017-04-12 15:51:29 +0200

    Exercice 21

--- schoolproject/src/main/java/schoolproject/App.java
+++ schoolproject/src/main/java/schoolproject/App.java
@@ -1,5 +1,9 @@
 package schoolproject;
 
+import java.io.*;
+import java.util.*;
+import com.opencsv.*;
+
 /**
  * Hello world!
  *
@@ -10,10 +14,34 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println(max(4,5));
+        
+        int monmax=0;
+        try {
+        	CSVReader reader =  new CSVReader(new FileReader("data.csv"));
+        	try {
+        		List<String[]> myEntries = reader.readAll();
+        		for (String[] s : myEntries) {
+        			
+        			System.out.println(Arrays.toString(s));
+					for(String nb : s){
+						//System.out.println("Nombre lu : "+ nb);
+                		monmax = max(monmax, Integer.parseInt(nb));
+					}
+        		}
+        		System.out.println("Mon max : " + monmax);
+        		
+        	}
+        	catch (IOException e) {
+        		System.out.println("standard message: IOE");
+        	}
+        }
+        catch(FileNotFoundException e) {
+        	System.out.println("standard message: FNFE");
+        }
     }
     
     public static int max(int a, int b){
-    	//return a > b ? a : b;
-    	return a; //Le stagiaire est passe par la
+    	return a > b ? a : b;
+    	//return a; //Le stagiaire est passe par la
     }
 }
